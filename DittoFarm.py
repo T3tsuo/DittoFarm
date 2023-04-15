9b967ad7951f95c3cb1bca8bbd6352cb72efa10b
Takumi Comeau <79553392+T3tsuo@users.noreply.github.com>
2023-04-15 17:25:35 -0300

    changed logic for ball_num

--- DittoFarm.py
+++ DittoFarm.py
@@ -11,9 +11,8 @@ def run(ball_num=None):
     if ball_num is None:
         ball_num = input("How many dusk balls do you have (number): ")
     ball_num = int(ball_num)
-    catch_ditto.set_ball_count(ball_num)
     time.sleep(2)
     # loop forever
     while True:
         heal_return.run()
-        catch_ditto.run()
+        ball_num = catch_ditto.run(ball_num)
